68bdef0faf120e0e4d7edf981c7704edf55d1625
Edoardo Rota <edoardorota96@gmail.com>
2018-12-01 21:37:56 +0100

    Added the ability to save and load games

--- lib/guess.rb
+++ lib/guess.rb
@@ -16,27 +16,48 @@ class Guess
   # and returns true if the user won (otherwise returns false). It also returns
   # true or false if at least a letter has been found
   def next_guess(letter)
-    i = 0
-    found = false
-
     if @already_asked.include?(letter)
       puts "You already tried this letter!"
       return false, true
     end
+
+    i = 0
+    found = false
+    @already_asked << letter
 
     @solution.each_char do |sol_letter|
       if sol_letter == letter
         @guess_state[i] = letter
         found = true
       end
-      @already_asked << letter
       i += 2  # doesn't count spaces between underscores
     end
 
-    if @guess_state == @solution
-      return true, found
-    else
-      return false, found
-    end
+    return @guess_state.gsub(/\s+/, "") == @solution, found
+  end
+
+  # Saves in a file the game. Takes as parameter the number of lives left
+  def save(lives)
+    puts "Name of the save file?"
+    str = gets.chomp
+
+    save_file = File.open("saves/#{str}.txt", "w")
+    save_file.puts @solution
+    save_file.puts @guess_state
+    save_file.puts lives
+    save_file.puts @already_asked.join(",")
+    save_file.close
+    puts "Game saved."
+  end
+
+  # Loads the file and returns the number of lives
+  def load
+    puts "Name of the save file to load?"
+    str = gets.chomp
+    save_file = File.readlines("saves/#{str}.txt")
+    @solution = save_file[0].strip
+    @guess_state = save_file[1].strip
+    @already_asked = save_file[3].strip.split(",")
+    return save_file[2].strip.to_i
   end
 end
--- lib/hangman.rb
+++ lib/hangman.rb
@@ -6,20 +6,36 @@ has_won = false
 
 puts "Welcome to Hangman!"
 
-while lives != 0 && !has_won
-  puts "\nType your next guess:"
-  has_won, found = game.next_guess(gets.chomp.downcase)
-  if !found
-    lives -= 1
-    puts "Your letter is not included, you lose a life."
-    puts "Remaining lives: #{lives}"
+while lives != 0
+  puts "\nType your next guess, type save to save the game or type load to load another game"
+  str = gets.chomp.downcase
+  while str.length != 1 && !str.match?(/save/) && !str.match?(/load/)
+    puts "Wrong input, try again"
+    str = gets.chomp.downcase
   end
-  puts game.guess_state
+
+  case str
+  when "save"
+    game.save(lives)
+    break
+  when "load"
+    lives = game.load
+  else
+    has_won, found = game.next_guess(str)
+    if !found
+      lives -= 1
+      puts "Your letter is not included, you lose a life."
+      puts "Remaining lives: #{lives}"
+    end
+    if has_won
+      puts "You won!"
+      break
+    end
+    puts game.guess_state
+  end
 end
 
-if has_won
-  puts "You won!"
-else
+if lives == 0
   puts "You lost."
   puts "The solution was #{game.solution}"
 end
